94e4c213a5b72554e3a16c426133c680ad313744
Sangjin Han <49024115+eu2525@users.noreply.github.com>
2024-02-04 17:19:16 +0900

    틀린 부분 수정

--- SWMembership/1916.cpp
+++ SWMembership/1916.cpp
@@ -6,18 +6,17 @@ using namespace std;
 // 각각의 간선의 정보를 pair를 통해서 저장
 vector<pair<int, int>> a[1001]; 
 int d[1001]; //각 정점에서의 최소비용
-
 void dijkstra(int start) {
 	d[start] = 0; //시작 지점의 비용은 0;
-	// priority_queue는 기본적으로 우선 순위가 높은 요소가 먼저 나오는 자료구조
-	// priority_queue<pair<int, int>>를 사용하면 두 번째 요소를 기준으로 우선 순위가 결정된다.
+    	// priority_queue는 기본적으로 우선 순위가 높은 요소가 먼저 나오는 자료구조
 	priority_queue<pair<int, int>> pq;
-	pq.push(make_pair(start, 0));
+    	//pq에 집어넣을때는 가중치를 first, 다음 노드 정보를 second에 넣음.
+	pq.push(make_pair(0, start));
 	//가까운 순서대로 처리하기 위해 큐를 이용
 	while (!pq.empty()) {
-		int current = pq.top().first;
-		//짧은 것이 먼저오게 음수화해서 저장.
-		int distance = -pq.top().second; //따라서 꺼낼때는 음수화를 풀어줌
+    	//짧은 것이 먼저오게 음수화해서 저장.
+		int distance = -pq.top().first; //따라서 꺼낼때는 음수화를 풀어줌
+		int current = pq.top().second;
 		pq.pop();
 		if (d[current] < distance) continue;
 		for (int i = 0; i < a[current].size(); i++) {
@@ -28,7 +27,7 @@ void dijkstra(int start) {
 			// 기존의 최소 비용보다 저렴하면 업데이트
 			if (nextDistance < d[next]) {
 				d[next] = nextDistance;
-				pq.push(make_pair(next, -nextDistance));
+				pq.push(make_pair(-nextDistance, next));
 			}
 		}
 	}
