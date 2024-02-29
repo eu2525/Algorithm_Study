a746086d1dd6c8e16e1ac313fc12e097d15cc7a3
Sangjin Han <49024115+eu2525@users.noreply.github.com>
2024-02-29 17:00:54 +0900

    Create 15649.cpp

---
+++ SWMembership/15649.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+vector<int> vec;
+bool visited[8] = { false };
+
+void dfs(int cnt) {
+	if (cnt == m) {
+		for (auto x : vec) {
+			cout << x << " ";
+		}
+		cout << '\n';
+		return;
+	}
+	else {
+		for (int i = 1; i <= n; i++) {
+			if (visited[i] == false) {
+				visited[i] = true;
+				vec.push_back(i);
+				dfs(cnt + 1);
+				vec.pop_back();
+				visited[i] = false;
+			}
+		}
+	}
+}
+
+int main() {
+	cin >> n >> m;
+
+	dfs(0);
+
+}
