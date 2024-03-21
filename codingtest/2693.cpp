c50e361c16af51405dd8dfb60299c64337d56e3f
Sangjin Han <49024115+eu2525@users.noreply.github.com>
2024-03-22 02:40:47 +0900

    Mergesort 구현

---
+++ codingtest/2693.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int sorted[11] = { 0 };
+
+void merge(int low, int mid, int high) {
+	vector<int> u;
+	int i = low;
+	int j = mid + 1;
+	while (i <= mid && j <= high) {
+		if (sorted[i] > sorted[j]) {
+			u.push_back(sorted[i]);
+			i++;
+		}
+		else {
+			u.push_back(sorted[j]);
+			j++;
+		}
+	}
+	for(int t = j; t <= high; t++)
+    u.push_back(sorted[t]);
+	for(int t = i; t <= mid; t++)
+		u.push_back(sorted[t]);
+	for(int t = low; t <= high; t++) {
+		sorted[t] = u[t - low];
+	}
+}
+
+void mergesort(int low, int high) {
+	if (low < high) {
+		int mid = (low + high) / 2;
+		mergesort(low, mid);
+		mergesort(mid + 1, high);
+		merge(low, mid, high);
+	}
+}
+
+int main() {
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+    for (int j = 0; j < 10; j++) {
+      cin >> sorted[j];
+    }
+	mergesort(0, 9);
+	cout << sorted[2] << '\n';
+  }
+}
